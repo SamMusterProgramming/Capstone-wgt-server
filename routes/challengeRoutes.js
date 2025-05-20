8e47403d757e2c4f1d8a78f7a3305deaddd3388f
samir <sam20232022@gmail.com>
2025-05-19 23:14:55 -0400

    notification

--- routes/challengeRoutes.js
+++ routes/challengeRoutes.js
@@ -111,10 +111,12 @@ route.post('/uploads',verifyJwt,async(req,res)=>{
             receiver_id:friend.sender_id,
             type:"followers",
             isRead:false,
-            message:req.body.privacy == "Private"?
-             req.body.friendList.find(fr => fr.sender_id == friend.sender_id) 
-                       ? "Invited you to a participate in his challenge" :"has create new Challenge",
-            content: {
+            message:
+            // req.body.privacy == "Private"?
+            //  req.body.friendList.find(fr => fr.sender_id == friend.sender_id) 
+                    //    ? "Invited you to a participate in his challenge" :
+                       "has create new Challenge",
+            content: {  
                 sender_id:req.body.origin_id,
                 challenge_id:newChallenge._id.toString(),
                 name:req.body.name,
