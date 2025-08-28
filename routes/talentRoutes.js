d4152485e77b17bf3baeaa97ec6fce0cf902a874
samir <sam20232022@gmail.com>
2025-08-28 02:58:16 -0400

    fix

--- routes/talentRoutes.js
+++ routes/talentRoutes.js
@@ -76,7 +76,7 @@ route.post('/creates',verifyJwt,async(req,res)=>{
      (edition.round >= 3))) {
         const roundDate = new Date(edition.updatedAt)
         const now = new Date();
-        const differenceInMilliseconds = (now - roundDate)/(1000*60*60*24*7)
+        const differenceInMilliseconds = (now - roundDate)/(1000*60*60*60)
         console.log(differenceInMilliseconds)
      
         if(differenceInMilliseconds >= 1) {
