6821faa73e33ecec71978a190ea8feb0b455ff5b
samir <sam20232022@gmail.com>
2025-09-16 22:50:37 -0400

    timelapse for roundes

--- routes/talentRoutes.js
+++ routes/talentRoutes.js
@@ -80,7 +80,7 @@ route.post('/creates',verifyJwt,async(req,res)=>{
      (edition.round >= 3))) {
         const roundDate = new Date(edition.updatedAt)
         const now = new Date();
-        const differenceInMilliseconds = (now - roundDate)/(1000*60)
+        const differenceInMilliseconds = (now - roundDate)/(1000*60*60*24*7)
         console.log(differenceInMilliseconds)
      
         if(differenceInMilliseconds >= 1) {
