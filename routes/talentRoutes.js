7cb50e663dc7971d61591dbb2eca32e2fa4d1c49
samir <sam20232022@gmail.com>
2026-02-09 10:08:56 -0500

    fix again

--- routes/talentRoutes.js
+++ routes/talentRoutes.js
@@ -696,8 +696,9 @@ route.post('/uploads/:id',verifyJwt,async(req,res)=>{
     const fileName = req.body.fileName
     const fileId = req.body.fileId
     console.log(fileName)
-    await b2.authorize();
-    const downloadUrl = b2.authorization.downloadUrl;
+    // await b2.authorize();
+    const auth = await b2.authorize();
+    const downloadUrl = auth.data.downloadUrl;
     const validForSeconds = 60 * 60 * 24 * 7; // 7 days
     const signedAuth = await b2.getDownloadAuthorization({
       bucketId: process.env.B2_BUCKET_ID,
