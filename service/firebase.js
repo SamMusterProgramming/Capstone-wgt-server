224d659cd1bb209014d4e549d9dcf892f9689f05
samir <sam20232022@gmail.com>
2026-03-25 03:42:52 -0400

    fix access key

--- service/firebase.js
+++ service/firebase.js
@@ -1,12 +1,12 @@
 import admin from 'firebase-admin';
-import serviceAccount from "../config/serviceAccountKey.json" assert { type: "json" };
-
+// import serviceAccount from "../config/serviceAccountKey.json" assert { type: "json" };
+console.log("PRIVATE KEY START:", process.env.FIREBASE_PRIVATE_KEY?.slice(0, 30));
 admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
     }),
   });
    
-export default admin;
+export default admin;  
