e556a8a01ea74aa3680a39fa555d5d4230d68dd0
samir <sam20232022@gmail.com>
2026-03-25 03:25:32 -0400

    add env var keys firebase

--- service/firebase.js
+++ service/firebase.js
@@ -1,9 +1,12 @@
 import admin from 'firebase-admin';
 import serviceAccount from "../config/serviceAccountKey.json" assert { type: "json" };
 
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
    
 export default admin;
