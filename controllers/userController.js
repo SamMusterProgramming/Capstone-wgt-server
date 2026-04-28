708e7b46d67722dccadac7b6e53f9342b8aaaca8
samir <sam20232022@gmail.com>
2026-04-28 02:09:29 -0400

    add controllers

---
+++ controllers/userController.js
@@ -0,0 +1,216 @@
+import friendModel from "../models/friends.js"
+import notificationModel from "../models/notifications.js"
+import talentModel from "../models/talent.js"
+import userModel from "../models/users.js"
+import { deleteFileFromB2_Public, getPublicUrlFromB2, getUploadPublicUrl } from "../utilities/blackBlazeb2.js"
+
+
+export const getUserById = async(req,res)=>{ // get single user by _id
+    const userId = req.params.id
+    const user = await userModel.findById(userId)
+    if(!user) return res.json({error:"cant find the user"}).status(404)
+    res.status(200).json(user)
+}
+
+export const deleteUserById = async(req,res)=>{ // delete single user by _id
+    const userId = req.params.id
+    const user = await userModel.findByIdAndDelete(userId)
+    if(!user) return res.json({error:"cant find the user"}).status(404)
+    res.status(200).json(user)
+  }
+
+  export const updateUserInfoById = async(req,res)=>{ 
+    console.log(req.body.name)
+    const userId = req.params.id
+    const user = await userModel.findByIdAndUpdate(
+             userId,
+            req.body,
+            { new: true }
+          )
+    if(!user) return res.json({error:"cant find the user"}).status(404)
+    res.status(200).json(user)
+  }
+
+
+  export const getUploadVideoUrl = async (req, res) => {
+    try {
+      const { userId ,name , type } = req.body;
+      // type = "profile" | "cover" | "post"
+      let fileName =  ""
+      if(type == "profile" || type == "cover") 
+           fileName =`users/${name.replace(/\s+/g, "")}_${userId}/${type}/${type}_${Date.now()}.jpg` 
+      if (type == "talent" || type == "challenge" )
+          fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}_contests/submission_${Date.now()}.mp4`
+      if (type == "thumbnail")  
+            fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}/thumbnail_${Date.now()}.jpg`;
+      const uploadUrlResponse = await getUploadPrivateUrl()
+      res.json({
+        uploadUrl: uploadUrlResponse.data.uploadUrl,
+        authorizationToken: uploadUrlResponse.data.authorizationToken,
+        fileName:fileName,
+      });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  export const getUploadImageUrl = async (req, res) => {
+    try {
+      const { userId ,name , type } = req.body;
+      console.log(userId + name)
+      // type = "profile" | "cover" | "post"
+      let fileName =  ""
+      if(type == "profile" || type == "cover") 
+           fileName =`users/${name.replace(/\s+/g, "")}_${userId}/${type}/${type}_${Date.now()}.jpg` 
+      // if (type == "talent" || type == "challenge" )
+      //     fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}_contests/submission_${Date.now()}.mp4`
+      if (type == "thumbnail")  
+            fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}/thumbnail_${Date.now()}.jpg`;
+      const uploadUrlResponse = await getUploadPublicUrl ()
+      res.json({
+        uploadUrl: uploadUrlResponse.data.uploadUrl,
+        authorizationToken: uploadUrlResponse.data.authorizationToken,
+        fileName:fileName,
+      });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+
+  export const saveProfileImage = async (req, res) => {
+    try {
+      const { userId, fileId, fileName, deleteFileId, deleteFileName } = req.body;
+  
+      if (!userId || !fileId || !fileName) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+      const signedUrl = await getPublicUrlFromB2(fileName);
+      const cdnUrl = signedUrl.replace(
+        "https://f005.backblazeb2.com",
+        "https://cdn.challenmemey.com"
+      );
+  
+      const updatedUser = await userModel.findByIdAndUpdate(
+        userId,
+        {
+          $set: {
+            profileImage: {
+              fileId,
+              fileName,
+              publicUrl: cdnUrl,
+            },
+          },
+        },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      if (deleteFileId && deleteFileName) {
+        deleteFileFromB2_Public(deleteFileName, deleteFileId)
+          .catch(err => console.error("Delete error:", err));
+      }
+  
+      await talentModel.updateMany(
+        {},
+        {
+          $set: {
+            "contestants.$[c].profile_img": cdnUrl,
+            "queue.$[q].profile_img": cdnUrl,
+            "eliminations.$[e].profile_img": cdnUrl,
+          },
+        },
+        {
+          arrayFilters: [
+            { "c.user_id": userId },
+            { "q.user_id": userId },
+            { "e.user_id": userId },
+          ],
+        }
+      );
+      let findFriend = await friendModel.findOne({ user_id: userId });
+      findFriend.profile_img = cdnUrl ; 
+      await findFriend.save()
+  
+      return res.json(updatedUser);
+  
+    } catch (err) {
+      console.error("SAVE PROFILE IMAGE ERROR:", err);
+      return res.status(500).json({ error: err.message });
+    }
+  }
+
+
+  export const saveCoverImage = async (req, res) => {
+    try {
+      const { userId, fileId, fileName, deleteFileId, deleteFileName } = req.body;
+  
+      if (!userId || !fileId || !fileName) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+      const signedUrl = await getPublicUrlFromB2(fileName);
+  
+      const cdnUrl = signedUrl.replace(
+        "https://f005.backblazeb2.com",
+        "https://cdn.challenmemey.com"
+      );
+      const updatedUser = await userModel.findByIdAndUpdate(
+        userId,
+        {
+          $set: {
+            coverImage: {
+              fileId,
+              fileName,
+              publicUrl: cdnUrl,
+            },
+          },
+        },
+        { new: true }
+      );
+  
+      let findFriend = await friendModel.findOne({ user_id: userId });
+      findFriend.cover_img = cdnUrl ; 
+      await findFriend.save()
+  
+      if (!updatedUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      // 3️⃣ Delete old file (NON-BLOCKING)
+      if (deleteFileId && deleteFileName) {
+        deleteFileFromB2_Public(deleteFileName, deleteFileId)
+          .catch(err => console.error("Delete error:", err));
+      }
+  
+      return res.json( updatedUser );
+  
+    } catch (err) {
+      console.error("SAVE PROFILE IMAGE ERROR:", err);
+      return res.status(500).json({ error: err.message });
+    }
+  }
+
+
+  export const getUserNotificationsByUserId = async(req,res)=>{
+    const receiver_id = req.params.id;
+    const notifications = await notificationModel.find({receiver_id:receiver_id}).sort({ createdAt: -1 });
+    res.json(notifications).status(200)
+  }
+
+  export const updateNotificationById = async(req,res)=>{
+    const _id = req.params.id;
+    const notification = await notificationModel.findById(_id)
+    if(!notification) return res.json("notification expired")
+    notification.isRead = true;
+    await notification.save();
+    res.json(notification).status(200)
+  }
+
+  export const deleteNotificationById  = async(req,res)=>{
+    const _id = req.params.id;
+    const notifications = await notificationModel.findByIdAndDelete(_id)
+    res.json("deleted").status(200)
+  }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,6 +19,7 @@ import { protect } from '../middleware/jwtProtect.js';
 import talentModel from '../models/talent.js';
 import { acceptRequest, cancelRequest, friendRequest, getFriendList, unfriendRequest } from '../controllers/friendController.js';
 import { followingRequest, getFollowersList, unfollowingRequest } from '../controllers/followController.js';
+import { deleteNotificationById, deleteUserById, getUploadImageUrl, getUploadVideoUrl, getUserById, getUserNotificationsByUserId, saveProfileImage, updateNotificationById, updateUserInfoById } from '../controllers/userController.js';
 // import admin from '../service/firebase.js';
 
 
@@ -33,6 +34,15 @@ route.get("/auth/me", protect, getMe);
 route.post("/auth/google", googleLogin);
 route.post("/auth/anonymous", anonymouslogin);
 
+// update user ,  name , profile image , cover , get user,  materials... 
+route.route('/user/:id')
+     .get(protect,validateMongoObjectId,getUserById)
+     .delete(protect,validateMongoObjectId,deleteUserById)
+     .patch(protect,validateMongoObjectId,updateUserInfoById)  
+route.post("/saveProfileImage",protect,saveProfileImage)
+route.post("/saveCoverImage",protect, saveProfileImage);
+route.post("/getUploadVideoUrl",protect, getUploadVideoUrl);
+route.post("/getUploadImageUrl", protect , getUploadImageUrl);
 
 //friends , requests ... 
 route.post("/friends/request/:id",protect,validateMongoObjectId,friendRequest);
@@ -46,184 +56,12 @@ route.post("/followings/add/:id",protect,validateMongoObjectId,followingRequest)
 route.patch('/unfollowing/:id',protect,validateMongoObjectId,unfollowingRequest)
 route.get('/follow/data/:id',protect,validateMongoObjectId,getFollowersList)
 
+// user notification
+route.route('/notifications/:id')
+     .get(protect , getUserNotificationsByUserId)
+     .patch(protect , updateNotificationById)  
+     .delete(protect,deleteNotificationById)  
 
-
-
-route.post("/getUploadVideoUrl", async (req, res) => {
-  try {
-    const { userId ,name , type } = req.body;
-    console.log(userId + name)
-    // type = "profile" | "cover" | "post"
-    let fileName =  ""
-    if(type == "profile" || type == "cover") 
-         fileName =`users/${name.replace(/\s+/g, "")}_${userId}/${type}/${type}_${Date.now()}.jpg` 
-    if (type == "talent" || type == "challenge" )
-        fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}_contests/submission_${Date.now()}.mp4`
-    if (type == "thumbnail")  
-          fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}/thumbnail_${Date.now()}.jpg`;
-    const uploadUrlResponse = await getUploadPrivateUrl()
-    res.json({
-      uploadUrl: uploadUrlResponse.data.uploadUrl,
-      authorizationToken: uploadUrlResponse.data.authorizationToken,
-      fileName:fileName,
-    });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-route.post("/getUploadImageUrl", async (req, res) => {
-  try {
-    const { userId ,name , type } = req.body;
-    console.log(userId + name)
-    // type = "profile" | "cover" | "post"
-    let fileName =  ""
-    if(type == "profile" || type == "cover") 
-         fileName =`users/${name.replace(/\s+/g, "")}_${userId}/${type}/${type}_${Date.now()}.jpg` 
-    // if (type == "talent" || type == "challenge" )
-    //     fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}_contests/submission_${Date.now()}.mp4`
-    if (type == "thumbnail")  
-          fileName = `users/${name.replace(/\s+/g, "")}_${userId}/${type}/thumbnail_${Date.now()}.jpg`;
-    const uploadUrlResponse = await getUploadPublicUrl ()
-    res.json({
-      uploadUrl: uploadUrlResponse.data.uploadUrl,
-      authorizationToken: uploadUrlResponse.data.authorizationToken,
-      fileName:fileName,
-    });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// route.post("/saveProfileImage", async (req, res) => {
-//   const { userId ,fileId , fileName, deleteFileId, deleteFileName} = req.body;
-//   const signedUrl = await getPublicUrlFromB2(fileName)
-//   await userModel.findByIdAndUpdate(userId, {
-//     profileImage: {
-//       fileId : fileId ,
-//       fileName :fileName,
-//       publicUrl : signedUrl,
-//     },
-//   });   
-//   console.log(deleteFileName)
-//   await deleteFileFromB2_Public(deleteFileName,deleteFileId)
-//   res.json({ signedUrl });
-// });
-
-route.post("/saveProfileImage", protect, async (req, res) => {
-  try {
-    const { userId, fileId, fileName, deleteFileId, deleteFileName } = req.body;
-
-    if (!userId || !fileId || !fileName) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-    console.log(deleteFileId, deleteFileName)
-    const signedUrl = await getPublicUrlFromB2(fileName);
-    const cdnUrl = signedUrl.replace(
-      "https://f005.backblazeb2.com",
-      "https://cdn.challenmemey.com"
-    );
-
-    const updatedUser = await userModel.findByIdAndUpdate(
-      userId,
-      {
-        $set: {
-          profileImage: {
-            fileId,
-            fileName,
-            publicUrl: cdnUrl,
-          },
-        },
-      },
-      { new: true }
-    );
-
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    if (deleteFileId && deleteFileName) {
-      deleteFileFromB2_Public(deleteFileName, deleteFileId)
-        .catch(err => console.error("Delete error:", err));
-    }
-
-    await talentModel.updateMany(
-      {},
-      {
-        $set: {
-          "contestants.$[c].profile_img": cdnUrl,
-          "queue.$[q].profile_img": cdnUrl,
-          "eliminations.$[e].profile_img": cdnUrl,
-        },
-      },
-      {
-        arrayFilters: [
-          { "c.user_id": userId },
-          { "q.user_id": userId },
-          { "e.user_id": userId },
-        ],
-      }
-    );
-    let findFriend = await friendModel.findOne({ user_id: userId });
-    findFriend.profile_img = cdnUrl ; 
-    await findFriend .save()
-
-    return res.json(updatedUser);
-
-  } catch (err) {
-    console.error("SAVE PROFILE IMAGE ERROR:", err);
-    return res.status(500).json({ error: err.message });
-  }
-});
-
-route.post("/saveCoverImage",protect,  async (req, res) => {
-  try {
-    const { userId, fileId, fileName, deleteFileId, deleteFileName } = req.body;
-
-    if (!userId || !fileId || !fileName) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-    const signedUrl = await getPublicUrlFromB2(fileName);
-
-    const cdnUrl = signedUrl.replace(
-      "https://f005.backblazeb2.com",
-      "https://cdn.challenmemey.com"
-    );
-    const updatedUser = await userModel.findByIdAndUpdate(
-      userId,
-      {
-        $set: {
-          coverImage: {
-            fileId,
-            fileName,
-            publicUrl: cdnUrl,
-          },
-        },
-      },
-      { new: true }
-    );
-
-    let findFriend = await friendModel.findOne({ user_id: userId });
-    findFriend.cover_img = cdnUrl ; 
-    await findFriend.save()
-
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // 3️⃣ Delete old file (NON-BLOCKING)
-    if (deleteFileId && deleteFileName) {
-      deleteFileFromB2_Public(deleteFileName, deleteFileId)
-        .catch(err => console.error("Delete error:", err));
-    }
-
-    return res.json( updatedUser );
-
-  } catch (err) {
-    console.error("SAVE PROFILE IMAGE ERROR:", err);
-    return res.status(500).json({ error: err.message });
-  }
-});
 
 // seeds the database with prototype data
 route.get('/seed',async(req,res)=>{
@@ -242,78 +80,6 @@ route.get('/followers/seed',async(req,res)=>{
 })
 
 
-
-route.route('/')
-   .get(async(req,res)=>{ // get all users
-     const users = await userModel.find({}).limit(20)
-     if(!users) return res.json({error:"users list is empty"})
-     res.json(users).status(200)   
-   })   
-
-   .post(validateUserRegistration,async(req,res)=> {  // add or register user
-      const user = {...req.body,name:req.body.firstname+" "+req.body.lastname}
-      const newUser = new userModel(user)
-      if(! newUser) return res.json({error:"can't save user"})
-      await newUser.save() 
-      const findFriend = await friendModel.findOne({user_id:user._id})  
-      if(!findFriend)  await  new friendModel(
-        {
-          user_id:newUser._id,
-          email:user.email,
-          name:user.name,
-          profile_img:user.profile_img
-      }).save() 
-      const findFollower = await followerModel.findOne({user_id:newUser._id})  
-      if(!findFollower)  await  new followerModel({
-                               user_id:newUser._id,
-                               email:user.email,
-                               profile_img:user.profile_img,
-                               name:user.name
-                        }).save()   
-       const id = newUser._id  
-       const  token = jwt.sign(
-          {id}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '23h' }
-       );
-        
-       res.status(200).json( {auth:true ,token:token , user:newUser})
-      // res.json(newUser).status(200)        
-   })   
-
-async function validateUserRegistration(req,res,next) {
-    if(!req.body.email || !req.body.password )
-       return res.status(404).json({error:"invalid entry"})
-    const query = {email:req.body.email}
-    const user = await userModel.findOne(query)
-    if(user) return res.json({error:"email exist already"}).status(400)
-    next()
-}  
-
-route.route('/users/:id')
-      .get(validateMongoObjectId,async(req,res)=>{ // get single user by _id
-        const userId = req.params.id
-        const user = await userModel.findById(userId)
-        if(!user) return res.json({error:"cant find the user"}).status(404)
-        res.status(200).json(user)
-      })
-      .delete(validateMongoObjectId,async(req,res)=>{ // delete single user by _id
-        const userId = req.params.id
-        const user = await userModel.findByIdAndDelete(userId)
-        if(!user) return res.json({error:"cant find the user"}).status(404)
-        res.status(200).json(user)
-      })
-      .patch(validateMongoObjectId, protect, async(req,res)=>{ // update user infos by _id
-        const userId = req.params.id
-        const updatedUser = await userModel.findByIdAndUpdate(
-            userId,
-            req.body,
-            { new: true }   
-        )
-        if(!updatedUser) return res.json({error:"cant find the user"}).status(404)
-        res.status(200).json(updatedUser)
-      })  
-
-
-
 function validateMongoObjectId(req,res,next) {
     if (!ObjectId.isValid(req.params.id)) return res.status(404).json({Error:"error in request ID"});
     next()
@@ -321,62 +87,19 @@ function validateMongoObjectId(req,res,next) {
 
        
   // challenge  get user by Id
-    route.get('/user/:id',protect,validateMongoObjectId, async(req,res)=>{ 
-      const userId = req.params.id
-      const user = await userModel.findById(userId)
-      if(!user) return res.json({error:"cant find the user"}).status(404)
-      res.status(200).json(user)
-    })
-
-    // update user 
-    route.patch('/user/:id',protect,validateMongoObjectId, async(req,res)=>{ 
-      console.log(req.body)
-      const userId = req.params.id
-      const user = await userModel.findByIdAndUpdate(
-               userId,
-              req.body,
-              { new: true }
-            )
-      if(!user) return res.json({error:"cant find the user"}).status(404)
-      res.status(200).json(user)
-    })
-               
-    
-    
-
-// followings
-route.get('/followings/:id',protect,validateMongoObjectId,async(req,res)=>{
-  const user_id = req.params.id
-  const followinglist = await followerModel.findOne({user_id:user_id})
-  if(!followinglist) return res.json([])
-  const followings = followinglist.followings;
-  return res.json(followings)
-})
+    // route.get('/user/:id',protect,validateMongoObjectId, async(req,res)=>{ 
+    //   const userId = req.params.id
+    //   const user = await userModel.findById(userId)
+    //   if(!user) return res.json({error:"cant find the user"}).status(404)
+    //   res.status(200).json(user)
+    // })
 
 
 
-//*********************** notifications */
 
-route.get('/notifications/:id',protect,async(req,res)=>{
-  const receiver_id = req.params.id;
-  const notifications = await notificationModel.find({receiver_id:receiver_id}).sort({ createdAt: -1 });
-  res.json(notifications).status(200)
-})  
 
-route.patch('/notifications/:id',protect,async(req,res)=>{
-  const _id = req.params.id;
-  const notification = await notificationModel.findById(_id)
-  if(!notification) return res.json("notification expired")
-  notification.isRead = true;
-  await notification.save();
-  res.json(notification).status(200)
-})  
 
-route.delete('/notifications/:id',protect,validateMongoObjectId,async(req,res)=>{
-  const _id = req.params.id;
-  const notifications = await notificationModel.findByIdAndDelete(_id)
-  res.json("deleted").status(200)
-})  
+
 
 
 //********************************* search for users */
@@ -387,79 +110,7 @@ route.get('/find/search',protect,async(req,res)=>{
   res.json({users:users})
 })   
 
-//****************************** login user here */
-route.post('/login', async(req, res)=>{     
- 
-    if(!req.body.email || !req.body.password) return res.json({error:"invalid loggin"}).status(404)
-    const query = {email:req.body.email.toLowerCase(),password:req.body.password}
-    const userEmail = await userModel.findOne({username:req.body.email.toLowerCase()})
-    if(!userEmail) return res.json({error:"user not found"}).status(404)
-    const user = await userModel.findOne(query)
-    if(!user) return res.json({error:"invalid password"}).status(404)
-    const findFollower = await followerModel.findOne({user_id:user._id})  
-    if(!findFollower)  await  new followerModel(
-            {
-              user_id:user._id,
-              email:user.email,
-              profile_img:user.profile_img,
-              name:user.name,
-              followers:[],
-              followings:[],
-            }
-          ).save()   
-    const findFriend = await friendModel.findOne({user_id:user._id})  
-    if(!findFriend)  await  new friendModel(
-      {
-        user_id:user._id,
-        email:user.email,
-        name:user.name,
-        profile_img:user.profile_img,
-        friend_request_sent:[],
-        friends:[]
-    }).save()   
-    const id = user._id  
-    const  token = jwt.sign(
-      {id}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '23h' }
-   );
-    
-   res.status(200).json( {auth:true ,token:token , user:user})
-})
-   
-//log out a user and reset session to null
-route.get('/logout',async(req,res)=>{
-    req.session.user = null  
-    req.session.save(function (err) {  
-      if (err) return err
-      req.session.regenerate(function (err) {
-        if (err) return err
-        return res.redirect('/')  
-      })
-    })   
-})
 
-// middleware to test if authenticated
-route.get('/isAuthenticated',protect ,async(req, res) =>{
-    const userId = req.user.id.toString()
-    const user =await  userModel.findById(userId)
-    res.json(user)
-}
-)
-     
-function validatePost(req,res,next) {
-      if(!req.body.id || !req.body.user_id || !req.body.image_url)
-         return res.status(404).json({error:"invalid entry"}) 
-      next()
-  }  
-    
-// function protect  (req,res,next){
-//   const token = req.headers.authorization?.split(' ')[1]; // Assuming token is sent in Authorization header
-//   if (!token) return res.status(401).json({ message: 'No token provided' });
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) return res.status(403).send({ message: 'Failed to authenticate token' });
-//     req.user = decoded; // Store decoded user information in the request object
-//     next();
-// });   
-// }
 
 
 export default route; 
